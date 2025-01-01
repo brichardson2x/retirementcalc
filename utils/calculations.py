4eaf177041f9c8c56e8f2a993f36baf2977a906f
Brandon Richardson <brichardson2x@gmail.com>
2024-12-31 19:02:27 -0600

    automated git

--- utils/calculations.py
+++ utils/calculations.py
@@ -48,22 +48,22 @@ def include_ssi(ssi_years, til_retire, inflation):
 
     return values
 
-def sum_benefits_calc(four01k_principal, ira_principal, hsa_principal, four01k_contribution, ira_contribution, hsa_contribution, dividend_principal, dividend_contribution, dividend_yield, years, rate, variance):
-    hsa_amount, hsa_values = definedbenefits_calc(hsa_principal, hsa_contribution, years, rate, variance)
-    ira_amount, ira_values = definedbenefits_calc(ira_principal, ira_contribution, years, rate, variance)
-    four01k_amount, four01k_values = definedbenefits_calc(four01k_principal, four01k_contribution, years, rate, variance)
-    dividends_amount, dividends_values = reinvest_dividend(dividend_principal, years, rate, dividend_yield, dividend_contribution, variance)
+def sum_benefits_calc(four01k_principal, ira_principal, hsa_principal, four01k_contribution, ira_contribution, hsa_contribution, dividend_principal, dividend_contribution, dividend_yield, til_retire, rate, variance):
+    hsa_amount, hsa_values = definedbenefits_calc(hsa_principal, hsa_contribution, til_retire, rate, variance)
+    ira_amount, ira_values = definedbenefits_calc(ira_principal, ira_contribution, til_retire, rate, variance)
+    four01k_amount, four01k_values = definedbenefits_calc(four01k_principal, four01k_contribution, til_retire, rate, variance)
+    dividends_amount, dividends_values = reinvest_dividend(dividend_principal, til_retire, rate, dividend_yield, dividend_contribution, variance)
 
     sum = hsa_amount + ira_amount + four01k_amount + dividends_amount
     sum_values = [hsa + ira + four01k + dividends for hsa, ira, four01k, dividends in zip(hsa_values, ira_values, four01k_values, dividends_values)]
     return sum, sum_values
 
-def spend_benefits(principal, rate, ssi_years, til_retire, inflation, til_death, variance, dividend_yield, ssi, spend_rate):
-    rate = rate - (variance**2)/2
+def spend_benefits(principal, rate, ssi_years, til_retire, inflation, til_death, variance, dividend_yield, spend_rate):
     dividends_values = []
     ssi_values = []
     values = []
     dividends_values = liveoff_dividend(principal, rate, dividend_yield, til_death, variance)
+    rate = rate - (variance**2)/2
     ssi_values = include_ssi(ssi_years, til_retire, inflation)
     for i in range(til_death):
         principal = principal*(1+rate) + ssi_values[i] + dividends_values[4 * i]- principal * spend_rate
@@ -74,3 +74,8 @@ def spend_benefits(principal, rate, ssi_years, til_retire, inflation, til_death,
 
     return values
 
+def graph_points(begin, retire, death, four01k_principal, ira_principal, hsa_principal, four01k_contribution, ira_contribution, hsa_contribution, dividend_principal, dividend_contribution, dividend_yield, rate, variance, inflation, spend_rate):
+    til_retire, til_death, ssi_years = years_retirement(begin, retire, death)
+    sum_principal, sum_values = sum_benefits_calc(four01k_principal, ira_principal, hsa_principal, four01k_contribution, ira_contribution, hsa_contribution, dividend_principal, dividend_contribution, dividend_yield, til_retire, rate, variance)
+    spend_values = spend_benefits(sum_principal, rate, ssi_years, til_retire, inflation, til_death, variance, dividend_yield, spend_rate)
+    return sum_principal, sum_values, spend_values
---
+++ utils/gitpush.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git add .
+git commit -m "automated git"
+git push
